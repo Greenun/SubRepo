1f842c51e29549640d28d59defe5382d96ee6614
West_Support <iopuy0234@gmail.com>
2020-09-09 16:19:34 +0900

    Update AmazonCredentials.java

--- web/src/main/java/com/swmaestro/web/auth/AmazonCredentials.java
+++ web/src/main/java/com/swmaestro/web/auth/AmazonCredentials.java
@@ -7,10 +7,10 @@ import software.amazon.awssdk.auth.credentials.*;
 @Component
 public class AmazonCredentials {
 
-    @Value("${aws.access.key.id:AKIAVO6B3BACCOYDKKGH}")
+    @Value("${aws.access.key.id}")
     private String awsId;
 
-    @Value("${aws.access.key.secret:IbpkLMlLh7DX2sQ4Bwn6uNBQIWkppZdl/Xu/ljdV}")
+    @Value("${aws.access.key.secret}")
     private String awsSecret;
 
     public AwsBasicCredentials getCredentials() {
